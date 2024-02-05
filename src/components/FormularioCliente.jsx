9846cf70adbb211e00183b576e9b0f696e023502
Fernando Herrera <herrera770@outlook.es>
2024-02-04 21:00:23 -0300

    se modificaron las opciones mostradas en el formulario

--- src/components/FormularioCliente.jsx
+++ src/components/FormularioCliente.jsx
@@ -18,7 +18,8 @@ const FormularioCliente = () => {
   //expresiones regulares
   const expRegNombreDuenio = /^[a-zA-Zá-úÁ-ÚüÜñÑ\s']+$/;
   const expRegNombreMascota = /^[a-zA-Zá-úÁ-ÚüÜñÑ\s']+$/;
-  const expRegFecha = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{10}$/;
+  const expRegFecha =
+    /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{10}$/;
   const expRegHora = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 
   function handleSubmit(e) {
@@ -33,8 +34,8 @@ const FormularioCliente = () => {
         ...citasAgendadas,
         { nombreMascota, nombreDuenio, fecha, hora, detalles, idCita },
       ]);
-    }else{
-      alert("Por favor, verifique los datos introducidos")
+    } else {
+      alert("Por favor, verifique los datos introducidos");
     }
   }
   function borrarCita(idUnica) {
@@ -46,63 +47,43 @@ const FormularioCliente = () => {
   return (
     <>
       <div className="container border my-4">
-        <h1 className="text-center">Administrador de pacientes</h1>
+        <h1 className="text-center">Administrador de películas</h1>
         <Form className="px-5" onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formNombreMascota">
-            <Form.Label>Nombre de la mascota</Form.Label>
+          <Form.Group className="mb-3" controlId="formTituloPelicula">
+            <Form.Label>Título de la película</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Introduzca el nombre de la mascota"
+              placeholder="Introducir el título completo"
               minLength={3}
-              maxLength={30}
+              maxLength={50}
               onChange={(e) => setNombreMascota(e.target.value)}
               value={nombreMascota}
               required
             ></Form.Control>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formNombreDueño">
-            <Form.Label>Nombre completo del dueño</Form.Label>
+          <Form.Group className="mb-3" controlId="formGenero">
+            <Form.Label>Género</Form.Label>
+            <Form.Select aria-label="select">
+              <option>Seleccionar género</option>
+              <option value="1">Acción</option>
+              <option value="2">Drama</option>
+              <option value="3">Comedio</option>
+              <option value="4">Infantil</option>
+              <option value="5">Animada</option>
+              <option value="6">Romance</option>
+              <option value="7">Terror</option>
+              <option value="8">Documental</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formDescripcion">
+            <Form.Label>Descripcion</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Introduzca el nombre completo del dueño"
+              placeholder="Introducir una breve descripción sobre la película"
               minLength={10}
               maxLength={80}
               onChange={(e) => setNombreDuenio(e.target.value)}
               value={nombreDuenio}
-              required
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formFecha">
-            <Form.Label>Fecha</Form.Label>
-            <Form.Control
-              type="date"
-              placeholder="dd/mm/aa"
-              onChange={(e) => setFecha(e.target.value)}
-              value={fecha}
-              required
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formHora">
-            <Form.Label>Hora (en formato 24 hs)</Form.Label>
-            <Form.Control
-              type="time"
-              placeholder="hh:mm"
-              minLength={5}
-              maxLength={5}
-              onChange={(e) => setHora(e.target.value)}
-              value={hora}
-              required
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formSintomas">
-            <Form.Label>Descripcion de síntomas</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Breve descripcion de los síntomas de la mascota"
-              minLength={10}
-              maxLength={500}
-              onChange={(e) => setDetalles(e.target.value)}
-              value={detalles}
               required
             ></Form.Control>
           </Form.Group>
